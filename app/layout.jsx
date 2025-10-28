24997cbdad44d2d5e2048ce00053ad7516d1b611
Tonydrdr <dr.anthonycosentino@gmail.com>
2025-10-28 08:22:14 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <head>
-        {/* CSS Leaflet pour la carte */}
+        {/<link
+  rel="stylesheet"
+  href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+  integrity="sha256-o9N1j7kGStb6Q2lJkGgGZ4Z1T1FQ0xS2JbZg9QxX0LE="
+  crossOrigin=""
+/>
+ CSS Leaflet pour la carte */}
         <link
           rel="stylesheet"
           href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
